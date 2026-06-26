9b85b9f6b853b0cfbcd4e4bb15ea2f5b56d60bf4
Mihai <me@dani.ro>
2026-06-26 22:16:40 +0200

    test(controller): cover payment aggregation and address dedup

---
+++ contracts/controller/tests/helpers/utils.rs
@@ -0,0 +1,65 @@
+use super::*;
+use controller_interface::types::Payment;
+use soroban_sdk::testutils::Address as _;
+use soroban_sdk::{Env, Vec};
+
+#[test]
+fn aggregate_payments_dedups_and_preserves_order() {
+    let env = Env::default();
+    let asset_a = Address::generate(&env);
+    let asset_b = Address::generate(&env);
+    let mut payments: Vec<Payment> = Vec::new(&env);
+    payments.push_back((asset_a.clone(), 10));
+    payments.push_back((asset_a.clone(), 5)); // same asset, summed
+    payments.push_back((asset_b.clone(), 3));
+
+    let out = aggregate_positive_payments(&env, &payments);
+
+    assert_eq!(out.len(), 2);
+    assert_eq!(out.get_unchecked(0), (asset_a, 15));
+    assert_eq!(out.get_unchecked(1), (asset_b, 3));
+}
+
+#[test]
+#[should_panic]
+fn aggregate_rejects_negative() {
+    let env = Env::default();
+    aggregate_payment_amount(&env, None, -1, false);
+}
+
+#[test]
+#[should_panic]
+fn aggregate_rejects_zero_when_not_withdraw_all() {
+    let env = Env::default();
+    aggregate_payment_amount(&env, None, 0, false);
+}
+
+#[test]
+fn aggregate_zero_is_withdraw_all_sentinel() {
+    let env = Env::default();
+    assert_eq!(aggregate_payment_amount(&env, None, 0, true), 0);
+    assert_eq!(aggregate_payment_amount(&env, Some(0), 5, true), 0);
+    assert_eq!(aggregate_payment_amount(&env, None, 5, true), 5);
+}
+
+#[test]
+fn aggregate_sums_previous_and_amount() {
+    let env = Env::default();
+    assert_eq!(aggregate_payment_amount(&env, Some(10), 5, false), 15);
+    assert_eq!(aggregate_payment_amount(&env, None, 7, false), 7);
+    assert_eq!(aggregate_payment_amount(&env, Some(0), 5, false), 5);
+}
+
+#[test]
+fn push_unique_dedups_preserving_order() {
+    let env = Env::default();
+    let a = Address::generate(&env);
+    let b = Address::generate(&env);
+    let mut out: Vec<Address> = Vec::new(&env);
+    push_unique_address(&mut out, a.clone());
+    push_unique_address(&mut out, a.clone());
+    push_unique_address(&mut out, b.clone());
+    assert_eq!(out.len(), 2);
+    assert_eq!(out.get_unchecked(0), a);
+    assert_eq!(out.get_unchecked(1), b);
+}
